59f50a06dea12a9915dec473cc536ae709154cc6
Ansh Jain <jainansh0610@gmail.com>
2024-08-04 00:26:29 +0530

    commit

--- mychat/settings.py
+++ mychat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xu*fy*6nojc@fuqaklm@w(nism(dexho^(el98-t%v2z=k4%w(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
